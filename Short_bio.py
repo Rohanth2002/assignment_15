96896a213b3012abc6c85a34f5449bc608084939
Rohanth2002 <rohanthgundu@gmail.com>
2023-11-06 10:51:01 +0530

    Initial Commit

---
+++ Short_bio.py
@@ -0,0 +1,12 @@
+name="Rohanth"
+print(name)
+hobbies="problem Solving","Doing Gym","Listening to Songs","Video Games","Dance"
+print(hobbies)
+favorite_food="Chicken"
+print(favorite_food)
+programming_experience=7
+print(programming_experience)
+used_python_before=True
+print(used_python_before)
+add = name , hobbies , favorite_food , programming_experience , used_python_before
+print(add)
